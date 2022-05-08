0433e4425cf1c349ed318828a476e64ac7981fba
shohayabt <shohayabhasan@yahoo.com>
2022-05-08 11:24:14 +0600

    signup and updating profile created

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,19 +1,63 @@
 import React from "react";
 import "./Signup.css";
+import { useState } from "react";
+import auth from "../../firebase";
+import {
+  useCreateUserWithEmailAndPassword,
+  useSendEmailVerification,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { useUpdateProfile } from "react-firebase-hooks/auth";
 
 const Signup = () => {
+  // UPDATING PROFILE
+  const [displayName, setDisplayName] = useState("");
+  const [updateProfile, updating, updateProfileError] = useUpdateProfile(auth);
+
+  // GOOGLE SIGN IN
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+  // SIGN IN WITH EMAIL AND PASSWORD
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  const [sendEmailVerification, sending, verificationError] =
+    useSendEmailVerification(auth);
   return (
     <section id="signup">
       <div className="container">
         <div className="signup-section">
-          <form className="signup-control">
+          <form
+            className="signup-control"
+            onSubmit={async (event) => {
+              event.preventDefault();
+              createUserWithEmailAndPassword(email, password);
+              await updateProfile({ displayName });
+              await sendEmailVerification();
+              console.log("VERIFICATION MAIL SENT");
+            }}
+          >
             <div className="input-area">
               <label htmlFor="name">Name:</label>
-              <input type="text" name="name" required placeholder="Name" />
+              <input
+                type="text"
+                name="name"
+                required
+                placeholder="Name"
+                onChange={(e) => setDisplayName(e.target.value)}
+              />
             </div>
             <div className="input-area">
               <label htmlFor="email">Email:</label>
-              <input type="email" name="email" required placeholder="Name" />
+              <input
+                type="email"
+                name="email"
+                required
+                placeholder="Name"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="input-area">
               <label htmlFor="password">Password:</label>
@@ -22,6 +66,7 @@ const Signup = () => {
                 name="password"
                 required
                 placeholder="Name"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <button type="submit" className="btn ">
